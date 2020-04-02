3c325e319373e38e06db4352c0aeb2d4338f6976
rickdouglas <rickmiranda23@gmail.com>
2020-04-02 18:24:17 -0300

    Upload e download de arquivos

--- 01 RestWithSpringBoot Calculator/src/main/java/br/com/erudio/services/FileStorageService.java
+++ 01 RestWithSpringBoot Calculator/src/main/java/br/com/erudio/services/FileStorageService.java
@@ -3,14 +3,18 @@ package br.com.erudio.services;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exception.FileStorageException;
+import br.com.erudio.exception.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -32,10 +36,32 @@ public class FileStorageService {
 		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 		
 		try {
+			if (fileName.contains("..")) {
+				throw new FileStorageException("Sorry, t	he file name contains an invalid path sequence " + fileName);
+			}
+			Path targetLocation = this.fileStorageLocation.resolve(fileName);
+			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			
 			return fileName;
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store file " + file + "Please try again", e);
 		}
 	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if(resource.exists()) {
+				return resource;
+			} else {
+				throw new MyFileNotFoundException("File not found " + fileName);
+			}
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found " + fileName, e);
+		}
+		
+	}
+
 	
 }
